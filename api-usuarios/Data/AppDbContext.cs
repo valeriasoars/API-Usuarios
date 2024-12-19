0565a6b879aeba5e94a606bf1d49787b448ee783
valeriasoars <valeriasoaressantos2@gmail.com>
2024-12-19 15:59:59 -0300

    database connection

---
+++ api-usuarios/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_usuarios.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
